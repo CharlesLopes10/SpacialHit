e661dd81ad953edf63a90cc26b4500904b5efe9e
CharlesLopes10 <charleslopes193@gmail.com>
2025-04-06 01:01:57 -0300

    Files

--- Code/Enemy.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-#-*- coding: utf-8 -*-
-from code.Const import ENTITY_SPEED, ENTITY_SHOT_DELAY
-from code.EnemyShot import EnemyShot
-from code.Entity import Entity
-
-class Enemy(Entity)
